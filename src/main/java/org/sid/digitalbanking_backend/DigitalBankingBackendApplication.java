561a1a62a16b984e13b6c8b948920c509a45d356
rabihrajaa <rajaarabihh@gmail.com>
2025-05-05 03:53:14 +0100

    commit1

---
+++ src/main/java/org/sid/digitalbanking_backend/DigitalBankingBackendApplication.java
@@ -0,0 +1,13 @@
+package org.sid.digitalbanking_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBankingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalBankingBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sid/digitalbanking_backend/DigitalBankingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.digitalbanking_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalBankingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
