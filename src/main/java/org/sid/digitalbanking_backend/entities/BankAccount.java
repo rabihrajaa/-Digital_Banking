0b438b28cb358d3e9b0658891b7bc685075a4de6
rabihrajaa <rajaarabihh@gmail.com>
2025-05-05 19:04:42 +0100

    insert data3

--- src/main/java/org/sid/digitalbanking_backend/entities/BankAccount.java
+++ src/main/java/org/sid/digitalbanking_backend/entities/BankAccount.java
@@ -9,8 +9,8 @@ import org.sid.digitalbanking_backend.enums.AccountStatus;
 import java.util.Date;
 import java.util.List;
 @Entity
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
-//@DiscriminatorColumn(name = "TYPE",length = 4)
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "TYPE",length = 4)
 @Data @NoArgsConstructor
 @AllArgsConstructor
 public abstract class  BankAccount {
