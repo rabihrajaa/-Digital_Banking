c63de0eb0e41eab71e4f0acf74351f69e58d6b36
rabihrajaa <rajaarabihh@gmail.com>
2025-05-05 16:01:01 +0100

    relations

--- src/main/java/org/sid/digitalbanking_backend/entities/AccountOperetion.java
+++ src/main/java/org/sid/digitalbanking_backend/entities/AccountOperetion.java
@@ -1,18 +1,22 @@
 package org.sid.digitalbanking_backend.entities;
 
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.sid.digitalbanking_backend.enums.OperationType;
 
 import java.util.Date;
+@Entity
 @Data @NoArgsConstructor
 @AllArgsConstructor
 public class AccountOperetion {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Date opeartionDate;
     private double amount;
     private OperationType type;
+    @ManyToOne
     private BankAccount bankAccount;
 }
--- src/main/java/org/sid/digitalbanking_backend/entities/BankAccount.java
+++ src/main/java/org/sid/digitalbanking_backend/entities/BankAccount.java
@@ -1,5 +1,9 @@
 package org.sid.digitalbanking_backend.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,13 +11,17 @@ import org.sid.digitalbanking_backend.enums.AccountStatus;
 
 import java.util.Date;
 import java.util.List;
+@Entity
 @Data @NoArgsConstructor
 @AllArgsConstructor
 public class BankAccount {
+    @Id
     private String id;
     private double balance;
     private Date CreatedAt;
     private AccountStatus status;
+    @ManyToOne
     private Customer customer;
+    @OneToMany(mappedBy = "bankAccount")
     private List<AccountOperetion> operetions;
 }
