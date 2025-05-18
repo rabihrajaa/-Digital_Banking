451abfa64659a0586ee18c14d6922c336f225375
rabihrajaa <rajaarabihh@gmail.com>
2025-05-18 03:02:28 +0100

    secrityConfig

---
+++ src/main/java/org/sid/digitalbanking_backend/security/SecurityConfig.java
@@ -0,0 +1,40 @@
+package org.sid.digitalbanking_backend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user1").password(encoder.encode("12345")).roles("USER").build(),
+                User.withUsername("admin").password(encoder.encode("12345")).roles("USER","ADMIN").build()
+        );
+    }
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(ar -> ar.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+}
